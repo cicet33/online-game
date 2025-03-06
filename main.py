1ff6dca31e17059e649b0f75d17f2fbd42ac3b15
egor <kamerad2121@gmail.com>
2025-03-06 12:15:54 +0500

    commit

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import pygame
 import os
+from sys import exit
 
 pygame.init()
 
-screen = create_screen()
-clock = pygame.time.Clock()            #get a pygame clock object
-player = load_player_image()
-background = load_background_image()
-screen.blit(background, (0, 0))        #draw the background
-position = player.get_rect()
-screen.blit(player, position)          #draw the player
-pygame.display.update()                #and show it all
-for x in range(100):                   #animate 100 frames
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('online game')
+clock = pygame.time.Clock()
+image_choose = input("choose the image: ")
+image = str(image_choose + ".jpg")
+surface = pygame.image.load(f"graphics/{image}")
 
-    screen.blit(background, position, position) #erase
+   
 
-    position = position.move(2, 0)     #move player
-
-    screen.blit(player, position)      #draw new player
-
-    pygame.display.update()            #and show it all
-
-    clock.tick(60)  
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.blit(surface,(100,100))
+    pygame.display.update()
+    clock.tick(60)
